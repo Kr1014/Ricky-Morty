a31cecfa34deaa5e409b30484ef8fa42d1006f14
Kelvin <kelvinrodriguez1409@gmail.com>
2025-04-09 15:02:56 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,66 @@
-import { useEffect, useRef, useState } from 'react'
-import './App.css'
-import useFetch from './hooks/useFetch'
-import LocationCards from './components/LocationCards'
-import ResidentsCards from './components/ResidentsCards'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import LocationCards from "./components/LocationCards";
+import ResidentsCards from "./components/ResidentsCards";
+import imageRicky from "./assets/imageRicky.png";
 
 function App() {
-  
-  const [infoLocation, getApi, isLoading, hasError] = useFetch()
-  const [search, setSearch] = useState(Math.floor(Math.random()*126+1))
-  
-  useEffect(() => {
-    const url = `https://rickandmortyapi.com/api/location/${search}`
-    getApi(url)
-  }, [search])
+  const [infoLocation, getApi, isLoading, hasError] = useFetch();
+  const [search, setSearch] = useState(Math.floor(Math.random() * 126 + 1));
 
+  useEffect(() => {
+    const url = `https://rickandmortyapi.com/api/location/${search}`;
+    getApi(url);
+  }, [search]);
 
   const textInput = useRef();
 
-  const handleSubmit = event =>{
-    event.preventDefault()
-    setSearch(textInput.current.value.trim())
-    event.target.reset()
-  }
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(textInput.current.value.trim());
+    event.target.reset();
+  };
 
   return (
-    <div className='container-all'>
-      {
-        isLoading ?
-          <h2 className='loading'>Loading....</h2>
-          :
+    <div className="container-all">
+      {isLoading ? (
+        <h2 className="loading">Loading....</h2>
+      ) : (
         <>
-        <div className="container-form">
-          <form onSubmit={handleSubmit} className='search'>
-                  <input type="text" min={1} max={126} ref={textInput} placeholder='Name of the location'/>
-                  <button>Search</button>
-          </form>
-        </div>
-          
-          {
-            hasError || search === '0' ?
-            <h2 className='err'>This location do not exist</h2>
-            :
+          <div className="containerImage">
+            <img src={imageRicky} alt="Rick and Morty" />
+          </div>
+          <div className="container-form">
+            <form onSubmit={handleSubmit} className="search">
+              <input
+                type="text"
+                min={1}
+                max={126}
+                ref={textInput}
+                placeholder="Name of the location"
+              />
+              <button>Search</button>
+            </form>
+          </div>
+
+          {hasError || search === "0" ? (
+            <h2 className="err">This location do not exist</h2>
+          ) : (
             <>
-              <LocationCards
-                infoLocation = {infoLocation}
-              /> 
+              <LocationCards infoLocation={infoLocation} />
 
-              <div className='app-container'>
-              {
-                infoLocation.residents.map(resident => (
-                  <ResidentsCards
-                    key = {resident}
-                    url = {resident}
-                  />
-                ))
-              }
+              <div className="app-container">
+                {infoLocation.residents.map((resident) => (
+                  <ResidentsCards key={resident} url={resident} />
+                ))}
               </div>
-
             </>
-          }
+          )}
         </>
-     }
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/LocationCards.jsx
+++ src/components/LocationCards.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const LocationCards = ({infoLocation, character}) => {
-
+const LocationCards = ({ infoLocation }) => {
   return (
-    <div className='container-all-information'>
-      <div className='information'>
-          <h1>{infoLocation?.name}</h1>
-          <ul className='list-info'>
-              <li className='info-item'><span>Type:</span>{infoLocation?.type}</li>
-              <li className='info-item'><span>Dimension:</span>{infoLocation?.dimension}</li>
-              <li className='info-item'><span>Population: </span>{infoLocation?.residents.length}</li>
-          </ul>
+    <div className="container-all-information">
+      <div className="information">
+        <h1>{infoLocation?.name}</h1>
+        <ul className="list-info">
+          <li className="info-item">
+            <span>Type:</span>
+            {infoLocation?.type}
+          </li>
+          <li className="info-item">
+            <span>Dimension:</span>
+            {infoLocation?.dimension}
+          </li>
+          <li className="info-item">
+            <span>Population: </span>
+            {infoLocation?.residents.length}
+          </li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LocationCards
+export default LocationCards;
